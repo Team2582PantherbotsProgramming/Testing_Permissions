1423a3dfeb4e3743006a9f3a0214d6c762da91c7
Ayruahs <federalistphicon@gmail.com>
2016-10-13 10:08:29 -0500

    Added test file to edit

---
+++ Testing Loops.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    cout << "Type in dog";
+    cin >> a;
+    if (a = "dog")
+    {
+        int x = 0;
+        while (x <= 5)
+        {
+            cout << x
+            x++;
+        }
+        cout << "You survived!\n";
+    }
+    else
+    {
+        cout << "You died...\n";
+        
+    
+}
